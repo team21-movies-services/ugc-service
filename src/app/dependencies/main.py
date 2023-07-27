3ab9e8d943eb32cf27e2c9d91712afc47959d556
Mikhail Spiridonov <mspiridonov2706@gmail.com>
2023-07-27 22:38:31 +0200

    fix: pretty log;

--- src/app/dependencies/main.py
+++ src/app/dependencies/main.py
@@ -8,13 +8,14 @@ from dependencies.mapping import dependencies_map
 logger = logging.getLogger(__name__)
 
 
-def _pretty_log(dependencies: dict, indent=0):
+def _pretty_log(dependencies: dict, indent=0) -> tuple:
     for key, value in dependencies.items():
         logger.info('\t' * indent + str(key))
         if isinstance(value, dict):
             _pretty_log(value, indent + 1)
         else:
             logger.info('\t' * (indent + 1) + str(value))
+    return tuple(dependencies.keys())
 
 
 def setup_dependencies(app: FastAPI, mapper: Optional[Dict[Any, Callable]] = None):
@@ -23,7 +24,6 @@ def setup_dependencies(app: FastAPI, mapper: Optional[Dict[Any, Callable]] = Non
         mapper = dependencies_map
     for interface, dependency in mapper.items():
         app.dependency_overrides[interface] = dependency
-
     logger.info("\nDependencies mapping: %s", _pretty_log(app.dependency_overrides))
 
     return None
