0c8b4479dbc4ccfb8310e875a6856b4dec6b838a
Pavel Khramov <102523130+KhramovKhramov@users.noreply.github.com>
2023-08-02 14:57:04 +0500

    chore: dependency with kafka service updated

--- src/app/dependencies/service_dependencies/views_service_faсtory.py
+++ src/app/dependencies/service_dependencies/views_service_faсtory.py
@@ -1,7 +1,11 @@
+from clients.kafka.kafka_client import KafkaService, get_kafka_service
 from dependencies.registrator import add_factory_to_mapper
+from fastapi import Depends
 from services.views import ViewsService, ViewsServiceABC
 
 
 @add_factory_to_mapper(ViewsServiceABC)
-def create_views_service() -> ViewsService:
-    return ViewsService()
+def create_views_service(
+    kafka_service: KafkaService = Depends(get_kafka_service),
+) -> ViewsService:
+    return ViewsService(kafka_service)
