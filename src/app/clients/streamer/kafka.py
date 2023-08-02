384344df3b9e7d0bb47784b80684ba4cf084f818
Pavel Khramov <102523130+KhramovKhramov@users.noreply.github.com>
2023-08-02 23:26:45 +0500

    feature: Kafka service updated with producer from depends

---
+++ src/app/clients/streamer/kafka.py
@@ -0,0 +1,13 @@
+from aiokafka import AIOKafkaProducer
+from clients.streamer.base import StreamerServiceABC
+
+
+class KafkaService(StreamerServiceABC):
+    def __init__(self, producer: AIOKafkaProducer) -> None:
+        self.producer = producer
+
+    async def send_message(self, topic: str, key: str, value: int) -> None:
+        # Отправка данных в Apache Kafka
+        await self.producer.send(topic=topic,
+                                 key=bytes(key, encoding='utf-8'),
+                                 value=bytes(str(value), encoding='utf-8'))
