c8626eb0f02f4307e75cb883190600178dc6f1b1
leonidbkh <leonidbkh@pm.me>
2023-08-04 13:49:09 +0400

    fix: constructor from consumer record added to MovieViewed

--- src/etl/models.py
+++ src/etl/models.py
@@ -1,5 +1,6 @@
 from time import time
 
+from aiokafka import ConsumerRecord
 from pydantic import BaseModel
 
 
@@ -9,4 +10,11 @@ class MovieViewed(BaseModel):
     user_id: str
 
     def transform_for_clickhouse(self) -> tuple:
-        return (self.user_id, self.film_id, self.viewed_frame, int(time()))
+        return self.user_id, self.film_id, self.viewed_frame, int(time())
+
+    @classmethod
+    def from_consumer_record(cls, record: ConsumerRecord):
+        key: bytes = record.key or b''
+        value: bytes = record.value or b''
+        user_id, film_id = key.decode().split('+')
+        return cls(user_id=user_id, film_id=film_id, viewed_frame=int(value.decode()))
