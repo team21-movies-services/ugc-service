8baf20673c7a77324d64398e0b552efd6464c066
leonidbkh <leonidbkh@pm.me>
2023-08-20 14:41:14 +0400

    fix router

--- src/app/dependencies/service_dependencies/__init__.py
+++ src/app/dependencies/service_dependencies/__init__.py
@@ -1,5 +1,5 @@
 from .auth_service_factory import create_auth_service
 from .favorites_service_factory import create_favorites_service
-from .saver_service_factory import create_saver_service
 from .status_service_factory import create_status_service
+from .user_action_service_factory import create_user_action_service
 from .views_service_factory import create_views_service
