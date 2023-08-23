8234fed83f01d8eb0cade6bf20e2aaee4c57c8cc
leonidbkh <leonidbkh@pm.me>
2023-08-23 23:09:12 +0400

    feature: delete method tests added

--- src/tests/functional/src/user_actions/test_delete.py
+++ src/tests/functional/src/user_actions/test_delete.py
@@ -2,35 +2,92 @@ from http import HTTPStatus
 
 import pytest
 from httpx import AsyncClient
+from motor.motor_asyncio import AsyncIOMotorCollection
 
 pytestmark = pytest.mark.asyncio
 
 
-@pytest.mark.parametrize(
-    "test_input",
-    [
-        {"action_type": "comment", "_id": "64e3d156f20cc6923d663806"},
-        {
-            "action_type": "favorite",
-            "user_id": "4bc7233d-c83e-4b6a-afc3-d025ecdf2397",
-            "film_id": "4bc7233d-c83e-4b6a-afc3-d025ecdf2397",
-        },
-        {
-            "action_type": "reaction",
-            "user_id": "42c7233d-c833-4b6a-afc3-d025ecdf2397",
-            "parent_id": "24e3676cec233c29b50d33fc",
-        },
-        {
-            "action_type": "rating",
-            "user_id": "4b37233d-c83e-4b6a-afc3-d025ecdf2397",
-            "parent_id": "4b37233d-c83e-4b6a-afc3-d025ecdf2397",
-        },
-    ],
-)
-async def test_delete_action(api_client: AsyncClient, test_input):
-    response = await api_client.request(url='/api/v1/actions/', method='DELETE', json=test_input)
+async def test_delete_comment(api_client: AsyncClient, mongo_collection: AsyncIOMotorCollection, make_comment):
+    result = await mongo_collection.insert_one(make_comment)
+    _id = str(result.inserted_id)
+    json = {"action_type": "comment", "_id": _id}
 
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+
+    assert status == HTTPStatus.NO_CONTENT
+
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
     status = response.status_code
 
     assert status == HTTPStatus.NOT_FOUND
-    # assert status == HTTPStatus.NO_CONTENT
+
+
+async def test_delete_reaction(
+    api_client: AsyncClient,
+    mongo_collection: AsyncIOMotorCollection,
+    make_reaction,
+):
+    await mongo_collection.insert_one(make_reaction)
+    user_id = make_reaction.get("user_id")
+    action_data = make_reaction.get("action_data")
+    parent_id = action_data.get("parent_id") if isinstance(action_data, dict) else False
+    json = {"action_type": "reaction", "user_id": user_id, "parent_id": parent_id}
+
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+
+    assert status == HTTPStatus.NO_CONTENT
+
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+    body = response.json()
+
+    assert body == {"detail": "Action not found"}
+    assert status == HTTPStatus.NOT_FOUND
+
+
+async def test_delete_rating(
+    api_client: AsyncClient,
+    mongo_collection: AsyncIOMotorCollection,
+    make_rating: dict,
+):
+    await mongo_collection.insert_one(make_rating)
+    user_id = make_rating.get("user_id")
+    action_data = make_rating.get("action_data")
+    parent_id = action_data.get("parent_id") if isinstance(action_data, dict) else False
+    json = {"action_type": "rating", "user_id": user_id, "parent_id": parent_id}
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+
+    assert status == HTTPStatus.NO_CONTENT
+
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+    body = response.json()
+
+    assert body == {"detail": "Action not found"}
+    assert status == HTTPStatus.NOT_FOUND
+
+
+async def test_delete_favorite(
+    api_client: AsyncClient,
+    mongo_collection: AsyncIOMotorCollection,
+    make_favorite: dict,
+):
+    await mongo_collection.insert_one(make_favorite)
+    user_id = make_favorite.get("user_id")
+    film_id = make_favorite.get("film_id")
+    json = {"action_type": "favorite", "film_id": film_id, "user_id": user_id}
+
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+
+    assert status == HTTPStatus.NO_CONTENT
+
+    response = await api_client.request(url='/api/v1/actions/', method="DELETE", json=json)
+    status = response.status_code
+    body = response.json()
+
+    assert body == {"detail": "Action not found"}
+    assert status == HTTPStatus.NOT_FOUND
